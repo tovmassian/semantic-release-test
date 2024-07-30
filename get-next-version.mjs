93f18b207429f9f26f59573da89fd2cd6ca85ad6
Tovmasyan, Khachatur [External] <Khachatur.Tovmasyan@externals.adidas.com>
2024-07-30 09:27:51 +0200

    fix: versin release false message

--- get-next-version.mjs
+++ get-next-version.mjs
@@ -20,7 +20,7 @@ async function getNextVersion() {
   console.log(
     result
       ? `VERSION_OUTPUT=${result.nextRelease.version}`
-      : 'Oups!, No release needed'
+      : 'Oeps!, No release needed'
   );
 }
 
